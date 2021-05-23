08e411af5990535dd4074c1a4268aa0e42c7ccdc
erickpauwels <erickpauwels@hotmail.com>
2021-05-23 19:18:34 -0300

    svg correction

--- js/misgifos.js
+++ js/misgifos.js
@@ -1,18 +1,43 @@
 
 
 function drawResultmyGif() {
+    let div = document.createElement('div');
     let url_blob = localStorage.getItem("migifo");
     let myGif = document.createElement('img');
     myGif.src = url_blob;
     myGif.classList.add ('my_gif');
-    myResults.appendChild(myGif);
+    myResults.appendChild(div);
+    myResults.appendChild(iconGifos);
+
+    div.appendChild(myGif);
     if (url_blob.length > 0) {
         myGifosText.style.display = "none";
         myGif.style.display = "flex";
     }else {
         myGifosText.style.display = "flex";
     }
+    iconGifos.style.display = "block";
 }
 
 drawResultmyGif();
 
+// -----------download function -------------------------- //
+
+iconGifos.addEventListener('click', () => {
+    let bajar = downloadGif();
+    console.log(bajar);
+})
+
+function downloadGif(url) {
+    fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => {
+        const imgHref = window.URL.createObjectURL(new Blob([blob]));
+        var link = document.createElement("a");
+        link.setAttribute("download", 'mygif.gif');
+        link.href = imgHref;
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+    });
+}
